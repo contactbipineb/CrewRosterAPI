ffa313556cadb74876739309ea8433f93c273423
contactbipineb <64191351+contactbipineb@users.noreply.github.com>
2020-10-11 15:35:41 +0400

    Final

--- Repository/DatabaseCrewRosterRepository.cs
+++ Repository/DatabaseCrewRosterRepository.cs
@@ -58,7 +58,6 @@ namespace EY.CabinCrew.Repositories
 
         private async Task<FlightDetails> GetFlightDetails(int planId)
         {
-
             var parameters = new { PlanId = planId };
             string commandText = "SELECT TOP 1 PK_FlightDetails as Id,[Blockhours]  as Blockhours,[FlightDepartueTime]  as FlightDepartueTime,[FlightArrivalTime]  as  FlightArrivalTime,[rest_hr]  as Rest_Hr, [trip_starttime_local]  as  FlightStartTime_Local,[trip_starttime_local_actual]  as FlightStartTime_Local_actual,[trip_crewcount]  as  CrewCount,cast([schd_dep_date] as date)  as   FlightStartDate,[flight_number]  as    SourceFlightCode, [TailNo]  as TailNo,[sub_fleet] as AcType,[SourceFlightCode]  asCode,[SourceCode]  as SourceCode,[DestinationCode]  as DestinationCode,[Port]  as Port,[dep_country]  as Source,[std_z_date]  as FlightStartDate_z,[std_ls_date]  as FlightStartDate_ls,[Z_FlightDepartueTime]  as FlightDepartueTime_z,[ls_FlightDepartueTime]  as  FlightDepartueTime_ls, [sta_z_date]  as   FlightArrDate_z, [Z_FlightArrivalTime]  as FlightArrivalTime_z, [ls_FlightArrivalTime]  as FlightArrivalTime_ls,[DepGateNumber]  as Dep_GateNumber,[ArrGateNumber]  as Arr_GateNumber FROM [HMS].[VW_FLIGHTDETAILS] where [PK_FlightDetails]= @PlanId ";
             //string sql = @"select top 1  Name,rank as Contact  from HMS.VW_PERSONALDETAILS where personal_id=@PersonalId";
